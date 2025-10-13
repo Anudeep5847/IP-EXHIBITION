c1e6d8474974862cd38c2737ab7138edcb317b30
Anudeep5847 <anudeeptirkey5847@gmail.com>
2025-10-13 17:19:00 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Add no-transition class to prevent transition on initial load
+document.body.classList.add('no-transition');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
