bccd546431c8990d5c3c41175208adcc1b976abf
Anudeep5847 <anudeeptirkey5847@gmail.com>
2025-10-13 18:52:23 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { Route, Switch } from "wouter";
 import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/Home";
 import Admin from "@/pages/Admin";
+import Login from "@/pages/Login";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
 
 function App() {
   return (
     <ThemeProvider>
       <Switch>
         <Route path="/" component={Home} />
-        <Route path="/admin" component={Admin} />
+        <Route path="/login" component={Login} />
+        <Route path="/admin">
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        </Route>
         <Route>
           <div className="min-h-screen flex items-center justify-center">
             <div className="text-center">
